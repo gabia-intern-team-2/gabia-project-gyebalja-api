baf9052ec495c5f0ac2d08335952b0578c9a9339
LEEHYUNJAE <35790290+hjjae2@users.noreply.github.com>
2020-01-31 17:38:52 +0900

    #24 테스트 코드 작성 - BoardRepository (기본적인 CRUD) - Rollback(true) 삭제

--- src/test/java/com/gabia/gyebalja/board/BoardRepositoryTest.java
+++ src/test/java/com/gabia/gyebalja/board/BoardRepositoryTest.java
@@ -16,7 +16,6 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
-@Rollback(true)
 @SpringBootTest
 public class BoardRepositoryTest {
     @PersistenceContext
