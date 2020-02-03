dc20f8b05179a938e4b02c074cba644184c96563
LEEHYUNJAE <35790290+hjjae2@users.noreply.github.com>
2020-02-03 11:02:16 +0900

    #37 테스트 코드 작성 - BoardImgRepository (기본적인 CRUD)

--- src/main/java/com/gabia/gyebalja/domain/BoardImg.java
+++ src/main/java/com/gabia/gyebalja/domain/BoardImg.java
@@ -22,4 +22,8 @@ public class BoardImg {
         this.board = board;
         this.imgPath = imgPath;
     }
+
+    public void changeImgPath(String imgPath){
+        this.imgPath = imgPath;
+    }
 }
