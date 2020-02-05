b167ec6c8b40326151ee1ce52875f2db9975581b
TaeGyun-Jung <wjdxorbs010@gmail.com>
2020-02-05 16:17:41 +0900

    테스트 코드 작성 - CategoryRepository, TagRepository, EduTagRepository (기본적인 CRUD) 및 엔티티 클래스 깃랩과 동기화

--- src/main/java/com/gabia/gyebalja/domain/User.java
+++ src/main/java/com/gabia/gyebalja/domain/User.java
@@ -1,20 +1,28 @@
 package com.gabia.gyebalja.domain;
 
-
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class User extends BaseTime {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //기본키 생성 전략중 데이터베이스 위임 'IDENTITY' 사용 (mysql)
     private Long id;
 
     @Column(nullable = false)
@@ -25,6 +33,10 @@ public class User extends BaseTime {
     @Column(length = 15, nullable = false)
     private String name;
 
+    /**
+     * enum 사용 시 Default가 ORDINARY인데 이것을 사용시 확장성에서도 문제가 생기며 유연하지 않음
+     * 꼭 EnumType.STRING 사용!
+     */
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private GenderType gender;
@@ -60,21 +72,9 @@ public class User extends BaseTime {
         this.department = department;
     }
 
-
-    /**
-     * 연관관계 메서드 세팅
-     * 유저의 부서가 변경된 경우
-     */
-//    public void changeDept(Department department) {
-//        this.department = department;
-//
-//        department.getUsers().add(this);
-//    }
-
+    //비밀번호 변경 비즈니스 로직
     public void changePassword(String password) {
         this.password = password;
     }
-
-
 
 }
---
+++ src/test/java/com/gabia/gyebalja/category/CatecoryRepositoryTest.java
@@ -0,0 +1,168 @@
+package com.gabia.gyebalja.category;
+
+import com.gabia.gyebalja.domain.Category;
+import com.gabia.gyebalja.repository.CategoryRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+public class CatecoryRepositoryTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Test
+    @DisplayName("Category 저장 테스트(save)")
+    public void saveTest() throws Exception {
+        //given
+        Long beforeCnt = categoryRepository.count();
+
+        Category category = Category.builder()
+                .name("개발자")
+                .build();
+
+        //when
+        categoryRepository.save(category);
+        em.clear();  //영속성 컨텍스트 초기화 ( 1차캐시에서 조회하는것을 막기위해)
+
+        Category findCategory = categoryRepository.findById(category.getId()).get();
+
+        //then
+        assertThat(findCategory.getId()).isEqualTo(category.getId());
+        assertThat(findCategory.getName()).isEqualTo(category.getName());
+        assertThat(categoryRepository.count()).isEqualTo(beforeCnt+1);
+
+    }
+
+    @Test
+    @DisplayName("Category 단건조회 테스트(findById)")
+    public void findByIdTest() throws Exception {
+        //given
+        Category category1 = Category.builder()
+                .name("개발자")
+                .build();
+        categoryRepository.save(category1);
+
+        Category category2 = Category.builder()
+                .name("기획자")
+                .build();
+        categoryRepository.save(category2);
+
+        em.clear();
+        //when
+        Category findCategory = categoryRepository.findById(category2.getId()).get();
+
+        //then
+        assertThat(findCategory.getId()).isEqualTo(category2.getId());
+        assertThat(findCategory.getName()).isEqualTo(category2.getName());
+    }
+
+    @Test
+    @DisplayName("Category 전체 조회 테스트(findAll)")
+    public void findAllTest() throws Exception {
+        //given
+        Category category1 = Category.builder()
+                .name("개발자")
+                .build();
+        categoryRepository.save(category1);
+
+        Category category2 = Category.builder()
+                .name("기획자")
+                .build();
+        categoryRepository.save(category2);
+
+        em.clear();
+        //when
+        List<Category> allCategory = categoryRepository.findAll();
+
+        //then
+        assertThat(allCategory.size()).isEqualTo(2);
+        assertThat(allCategory.get(0).getId()).isEqualTo(category1.getId());
+        assertThat(allCategory.get(0).getName()).isEqualTo(category1.getName());
+    }
+
+    @Test
+    @DisplayName("Category 갯수 테스트(count)")
+    public void countTest() throws Exception {
+        //given
+        Category category1 = Category.builder()
+                .name("개발자")
+                .build();
+        categoryRepository.save(category1);
+
+        Category category2 = Category.builder()
+                .name("기획자")
+                .build();
+        categoryRepository.save(category2);
+        //when
+        long count = categoryRepository.count();
+        //then
+        assertThat(count).isEqualTo(2);
+
+    }
+
+    @Test
+    @DisplayName("Category 삭제 테스트(delete")
+    public void deleteTest() throws Exception {
+        //given
+        Category category1 = Category.builder()
+                .name("개발자")
+                .build();
+        categoryRepository.save(category1);
+
+        Category category2 = Category.builder()
+                .name("기획자")
+                .build();
+        categoryRepository.save(category2);
+
+        long beforeDeleteCnt = categoryRepository.count();
+        //when
+        categoryRepository.delete(category1);
+        //then
+        assertThat(categoryRepository.count()).isEqualTo(beforeDeleteCnt-1);
+        assertThat(categoryRepository.findById(category1.getId())).isEqualTo(Optional.empty());
+
+    }
+
+    @Test
+    @DisplayName("Category 업데이트 테스트(update)")
+    public void updateTest() throws Exception {
+        //given
+        String updateName = "이름 업데이트";
+        
+        Category category = Category.builder()
+                                    .name("개발자")
+                                    .build();
+        categoryRepository.save(category);
+        long beforeUpdateCnt = categoryRepository.count();
+        //when
+        category.changeCategoryName(updateName);
+        em.flush();
+        Category findCategory = categoryRepository.findById(category.getId()).get();
+        
+        //then
+        assertThat(findCategory.getId()).isEqualTo(category.getId());
+        assertThat(findCategory.getName()).isEqualTo(updateName);
+        assertThat(categoryRepository.count()).isEqualTo(beforeUpdateCnt);
+
+        
+    }
+        
+
+
+}
