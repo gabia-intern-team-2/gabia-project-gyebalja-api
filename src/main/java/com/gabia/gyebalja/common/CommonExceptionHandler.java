0e4619931408dbebfb457f247932cc0ccad13001
LEEHYUNJAE <hjjae2@gmail.com>
2020-02-07 17:31:47 +0900

    #77 예외처리 - 어노테이션 누락 수정

--- src/main/java/com/gabia/gyebalja/common/CommonExceptionHandler.java
+++ src/main/java/com/gabia/gyebalja/common/CommonExceptionHandler.java
@@ -4,9 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+@ControllerAdvice
 public class CommonExceptionHandler {
     /**
      *  IllegalArgumentException.class
