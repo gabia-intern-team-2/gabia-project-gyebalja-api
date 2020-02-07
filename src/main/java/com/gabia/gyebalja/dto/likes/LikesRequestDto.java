63bcbce9e2a6268f5ff1afd1aa8f23e27a18f227
LEEHYUNJAE <35790290+hjjae2@users.noreply.github.com>
2020-02-07 09:29:49 +0900

    #71 Api 구현 - Likes > CRUD - Dto, Repository 작성

--- src/main/java/com/gabia/gyebalja/dto/likes/LikesRequestDto.java
+++ src/main/java/com/gabia/gyebalja/dto/likes/LikesRequestDto.java
@@ -1,4 +1,23 @@
 package com.gabia.gyebalja.dto.likes;
 
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.util.Assert;
+
+@ToString
+@Getter
 public class LikesRequestDto {
+
+    private Long userId;
+    private Long boardId;
+
+    @Builder
+    public LikesRequestDto(Long userId, Long boardId){
+        Assert.notNull(userId, "UserId must not be null");
+        Assert.notNull(boardId, "BoardId must not be null");
+        this.userId = userId;
+        this.boardId = boardId;
+    }
 }
