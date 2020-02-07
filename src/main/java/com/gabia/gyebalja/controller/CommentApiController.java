049c7093ee07c1101d33c31be1caa1b1d4e06bad
LEEHYUNJAE <35790290+hjjae2@users.noreply.github.com>
2020-02-07 17:42:59 +0900

    #68 Api 구현 - Comment > CRUD (#72) (#79)
    * #68 Api 구현 - Comment > CRUD 서비스, 서비스 테스트 코드 작성
    
    * #68 Api 구현 - Comment > CRUD 컨트롤, 컨트롤 테스트 코드 작성

--- src/main/java/com/gabia/gyebalja/controller/CommentApiController.java
+++ src/main/java/com/gabia/gyebalja/controller/CommentApiController.java
@@ -1,45 +1,51 @@
-//package com.gabia.gyebalja.controller;
-//
-//import com.gabia.gyebalja.dto.comment.CommentRequestDto;
-//import com.gabia.gyebalja.dto.comment.CommentResponseDto;
-//import com.gabia.gyebalja.service.CommentService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RequiredArgsConstructor
-//@RestController
-//public class CommentApiController {
-//
-//    private final CommentService commentService;
-//
-//    /** 등록 - comment 한 건 */
-//    @PostMapping("/api/v1/comments")
-//    public Long postOneComment(@RequestBody CommentRequestDto commentRequestDto){
-//        Long commentId = commentService.save(commentRequestDto);
-//
-//        return commentId;
-//    }
-//
-//    /** 조회 - comment 한 건 */
-//    @GetMapping("/api/v1/comments/{id}")
-//    public CommentResponseDto getOneComment(@PathVariable("id") Long id){
-//        CommentResponseDto commentResponseDto = commentService.findById(id);
-//
-//        return commentResponseDto;
-//    }
-//
-//    /** 수정 - comment 한 건 */
-//    @PutMapping("/api/v1/comments/{id}")
-//    public Long putOneComment(@PathVariable("id") Long id, @RequestBody CommentRequestDto commentRequestDto){
-//        Long commentId = commentService.update(id, commentRequestDto);
-//
-//        return commentId;
-//    }
-//
-//    @DeleteMapping("/api/v1/comments/{id}")
-//    public Long deleteOneComment(@PathVariable("id") Long id){
-//        commentService.delete(id);  // 검토. try - catch?
-//
-//        return 200L;    // 검토. return 값 무엇으로?
-//    }
-//}
+package com.gabia.gyebalja.controller;
+
+import com.gabia.gyebalja.dto.comment.CommentRequestDto;
+import com.gabia.gyebalja.dto.comment.CommentResponseDto;
+import com.gabia.gyebalja.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class CommentApiController {
+
+    private final CommentService commentService;
+
+    /** 등록 - comment 한 건 */
+    @PostMapping("/api/v1/comments")
+    public Long postOneComment(@RequestBody CommentRequestDto commentRequestDto){
+        Long commentId = commentService.save(commentRequestDto);
+
+        return commentId;
+    }
+
+    /** 조회 - comment 한 건 */
+    @GetMapping("/api/v1/comments/{id}")
+    public CommentResponseDto getOneComment(@PathVariable("id") Long id){
+        CommentResponseDto commentResponseDto = commentService.findById(id);
+
+        return commentResponseDto;
+    }
+
+    /** 수정 - comment 한 건 */
+    @PutMapping("/api/v1/comments/{id}")
+    public Long putOneComment(@PathVariable("id") Long id, @RequestBody CommentRequestDto commentRequestDto){
+        Long commentId = commentService.update(id, commentRequestDto);
+
+        return commentId;
+    }
+
+    @DeleteMapping("/api/v1/comments/{id}")
+    public Long deleteOneComment(@PathVariable("id") Long id){
+        commentService.delete(id);  // 검토. try - catch?
+
+        return 200L;    // 검토. return 값 무엇으로?
+    }
+}
--- src/main/java/com/gabia/gyebalja/service/CommentService.java
+++ src/main/java/com/gabia/gyebalja/service/CommentService.java
@@ -1,51 +1,72 @@
-//package com.gabia.gyebalja.service;
-//
-//import com.gabia.gyebalja.domain.Comment;
-//import com.gabia.gyebalja.dto.comment.CommentRequestDto;
-//import com.gabia.gyebalja.dto.comment.CommentResponseDto;
-//import com.gabia.gyebalja.repository.CommentRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@RequiredArgsConstructor
-//@Service
-//public class CommentService {
-//
-//    private final CommentRepository commentRepository;
-//
-//    /** 등록 - comment 한 건 (댓글 등록) */
-//    @Transactional
-//    public Long save(CommentRequestDto commentRequestDto){
-//        System.out.println("SERVICE START");
-//        Long commentId = commentRepository.save(commentRequestDto.toEntity()).getId();
-//        System.out.println("SERVICE END");
-//        return commentId;
-//    }
-//
-//    /** 조회 - comment 한 건 (어디서 사용할 지 모르지만) */
-//    @Transactional
-//    public CommentResponseDto findById(Long id){
-//        Comment comment = commentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다."));
-//
-//        return new CommentResponseDto(comment);
-//    }
-//
-//    /** 수정 - comment 한 건 */
-//    @Transactional
-//    public Long update(Long id, CommentRequestDto commentRequestDto){
-//        Comment comment = commentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다."));
-//        // 더티 체킹
-//        comment.changeContent(commentRequestDto.getContent());
-//
-//        return id;
-//    }
-//
-//    /** 삭제 - comment 한 건 */
-//    @Transactional
-//    public Long delete(Long id){
-//        commentRepository.deleteById(id);
-//
-//        return id;
-//    }
-//}
+package com.gabia.gyebalja.service;
+
+import com.gabia.gyebalja.domain.Board;
+import com.gabia.gyebalja.domain.Comment;
+import com.gabia.gyebalja.domain.User;
+import com.gabia.gyebalja.dto.comment.CommentRequestDto;
+import com.gabia.gyebalja.dto.comment.CommentResponseDto;
+import com.gabia.gyebalja.repository.BoardRepository;
+import com.gabia.gyebalja.repository.CommentRepository;
+import com.gabia.gyebalja.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+
+    @PersistenceContext
+    EntityManager em;
+
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
+
+    /** 등록 - comment 한 건 (댓글 등록) */
+    @Transactional
+    public Long save(CommentRequestDto commentRequestDto){
+
+        User user = userRepository.findById(commentRequestDto.getUserId()).orElseThrow(() -> new IllegalArgumentException("해당 데이터가 없습니다."));
+        Board board = boardRepository.findById(commentRequestDto.getBoardId()).orElseThrow(() -> new IllegalArgumentException("해당 데이터가 없습니다."));
+
+        Long commentId = commentRepository.save(Comment.builder().content(commentRequestDto.getContent()).user(user).board(board).build()).getId();
+        // Long commentId = commentRepository.save(commentRequestDto.toEntity()).getId();
+
+        return commentId;
+    }
+
+    /** 조회 - comment 한 건 (어디서 사용할 지 모르지만 일단 구현) */
+    @Transactional
+    public CommentResponseDto findById(Long id){
+        Comment comment = commentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다."));
+
+        return new CommentResponseDto(comment);
+    }
+
+    /** 수정 - comment 한 건 */
+    @Transactional
+    public Long update(Long id, CommentRequestDto commentRequestDto){
+        Comment comment = commentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다."));
+
+        // 더티 체킹
+        comment.changeContent(commentRequestDto.getContent());
+        em.flush();
+        em.clear();
+
+        return id;
+    }
+
+    /** 삭제 - comment 한 건 */
+    @Transactional
+    public Long delete(Long id){
+        commentRepository.deleteById(id);
+        em.flush();
+        em.clear();
+
+        return id;
+    }
+}
