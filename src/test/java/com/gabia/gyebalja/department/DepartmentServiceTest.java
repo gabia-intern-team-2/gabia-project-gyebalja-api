d380ecaf9ba43c24cdadb248fa303d36e0ef0b3e
LEEHYUNJAE <35790290+hjjae2@users.noreply.github.com>
2020-02-10 09:37:02 +0900

    #86 Api 작성 - Department > 서비스 작성 - 서비스, 서비스 테스트 (#87)
    #86 Api 작성 - Department > 서비스 작성
    
    - 서비스 작성
    
    - 서비스 테스트코드 작성
    
    (조회 부분만 작성)

---
+++ src/test/java/com/gabia/gyebalja/department/DepartmentServiceTest.java
@@ -0,0 +1,57 @@
+package com.gabia.gyebalja.department;
+
+import com.gabia.gyebalja.domain.Department;
+import com.gabia.gyebalja.dto.department.DepartmentResponseDto;
+import com.gabia.gyebalja.repository.DepartmentRepository;
+import com.gabia.gyebalja.service.DepartmentService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+public class DepartmentServiceTest {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private Department department;
+
+    @Autowired
+    public DepartmentServiceTest(){
+        // Department
+        this.department = Department.builder()
+                .name("테스트 - 부서")
+                .depth(0)
+                .parentDepartment(null)
+                .build();
+    }
+
+    @Test
+    @DisplayName("DepartmentService.findById() 테스트 (단건 조회)")
+    public void findTest(){
+        // given
+        Long saveId = departmentRepository.save(department).getId();
+        em.flush();
+        em.clear();
+
+        // when
+        DepartmentResponseDto departmentResponseDto = departmentService.findById(saveId);
+
+        // then
+        assertThat(departmentResponseDto.getId()).isEqualTo(saveId);
+    }
+}
