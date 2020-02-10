ca8c35b2c452f9ef4d9f732d9fd89a0290de4396
LEEHYUNJAE <35790290+hjjae2@users.noreply.github.com>
2020-02-10 13:06:49 +0900

    #101 Api 작성 - Department > 서비스 단 메소드명 변경 (#102)
    * #84 Api 작성 - Department > 컨트롤러 작성 (#85)
    
    #84 Api 작성 - Department > 컨트롤러 작성
    
    - 컨트롤러 작성
    
    - 컨트롤러 테스트코드 작성
    
    (조회 부분만 작성)
    
    * #86 Api 작성 - Department > 서비스 작성 - 서비스, 서비스 테스트 (#87)
    
    #86 Api 작성 - Department > 서비스 작성
    
    - 서비스 작성
    
    - 서비스 테스트코드 작성
    
    (조회 부분만 작성)
    
    * #88 Api 작성 - Department > DTO 작성 (#89)
    
    #88 Api 작성 - Department > DTO 작성
    
    - 리스폰스 DTO 작성 (DepartmentResponseDto)
    
    - 리퀘스트 DTO 작성 (파일만 생성)
    
    (아직까지 리퀘스트 DTO의 경우는 필요 없을 것 같아 작성하지 않음)
    
    * #83 Api 작성 - Department - Merge 후 코드 수정
    
    * #101 Api 작성 - Department > 서비스 단 메소드명 변경
    
    * findTest -> getOneDepartmentTest

--- src/main/java/com/gabia/gyebalja/common/StatusCode.java
+++ src/main/java/com/gabia/gyebalja/common/StatusCode.java
@@ -1,9 +1,17 @@
 package com.gabia.gyebalja.common;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
-public enum ErrorCode {
+public enum StatusCode {
+
+    // --- 2xx Success ---
+    OK("200", "OK"),
+    CREATED("201", "Created"),
+    ACCEPTED("202", "Accepted"),
+    NO_CONTENT("204", "No Content"),
+
 
     // --- 4xx Client Error ---
     BAD_REQUEST("400", "BAD_REQUEST"),
@@ -11,8 +19,10 @@ public enum ErrorCode {
     NOT_FOUND("404", "NOT_FOUND"),
     METHOD_NOT_ALLOWED("405", "METHOD_NOT_ALLOWED"),
 
+
     // --- 5xx Server Error ---
     INTERNAL_SERVER_ERROR("500", "INTERNAL_SERVER_ERROR");
+
 
     // --- 여기에 추가적인 예외 코드 생성 ---
     // Board
@@ -23,7 +33,7 @@ public enum ErrorCode {
     private final String code;
     private final String message;
 
-    ErrorCode(final String code, final String message){
+    StatusCode(final String code, final String message){
         this.code = code;
         this.message = message;
     }
