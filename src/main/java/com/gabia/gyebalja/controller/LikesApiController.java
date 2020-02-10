23d0850bdac1cf88cd1a0d411713c4703e12f33a
LEEHYUNJAE <35790290+hjjae2@users.noreply.github.com>
2020-02-10 09:40:39 +0900

    #91 Api 작성 - Likes > CommonJsonFormat 적용 (#94)
    #91 Api 작성 - Likes > CommonJsonFormat 적용
    
    - CommonJsonFormat 적용

--- src/main/java/com/gabia/gyebalja/controller/LikesApiController.java
+++ src/main/java/com/gabia/gyebalja/controller/LikesApiController.java
@@ -1,6 +1,9 @@
 package com.gabia.gyebalja.controller;
 
+import com.gabia.gyebalja.common.CommonJsonFormat;
+import com.gabia.gyebalja.common.StatusCode;
 import com.gabia.gyebalja.dto.likes.LikesRequestDto;
+import com.gabia.gyebalja.dto.likes.LikesResponseDto;
 import com.gabia.gyebalja.service.LikesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,17 +22,17 @@ public class LikesApiController {
 
     /** 등록 - likes 한 개 */
     @PostMapping("/api/v1/likes")
-    public Long postOneLikes(@RequestBody LikesRequestDto likesRequestDto){
-        Long likesId = likesService.save(likesRequestDto);
+    public CommonJsonFormat postOneLikes(@RequestBody LikesRequestDto likesRequestDto){
+        LikesResponseDto likesResponseDto = likesService.save(likesRequestDto);
 
-        return likesId;
+        return new CommonJsonFormat(StatusCode.OK.getCode(),StatusCode.OK.getMessage(),likesResponseDto);
     }
 
     /** 삭제 - likes 한 개 */
     @DeleteMapping("/api/v1/likes/users/{userId}/boards/{boardId}")
-    public Long deleteOneLikes(@PathVariable("userId") Long userId, @PathVariable("boardId") Long boardId){
+    public CommonJsonFormat deleteOneLikes(@PathVariable("userId") Long userId, @PathVariable("boardId") Long boardId){
         likesService.delete(userId, boardId);
 
-        return 200L;
+        return new CommonJsonFormat(StatusCode.NO_CONTENT.getCode(), StatusCode.NO_CONTENT.getMessage(), new ArrayList<>());
     }
 }
--- src/main/java/com/gabia/gyebalja/service/LikesService.java
+++ src/main/java/com/gabia/gyebalja/service/LikesService.java
@@ -4,6 +4,7 @@ import com.gabia.gyebalja.domain.Board;
 import com.gabia.gyebalja.domain.Likes;
 import com.gabia.gyebalja.domain.User;
 import com.gabia.gyebalja.dto.likes.LikesRequestDto;
+import com.gabia.gyebalja.dto.likes.LikesResponseDto;
 import com.gabia.gyebalja.repository.BoardRepository;
 import com.gabia.gyebalja.repository.LikesRepository;
 import com.gabia.gyebalja.repository.UserRepository;
@@ -15,6 +16,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class LikesService {
 
@@ -26,24 +28,22 @@ public class LikesService {
     private final BoardRepository boardRepository;
 
     /** 등록 - likes 한 개 */
-    @Transactional
-    public Long save(LikesRequestDto likesRequestDto){
+    public LikesResponseDto save(LikesRequestDto likesRequestDto){
         User user = userRepository.findById(likesRequestDto.getUserId()).orElseThrow(() -> new IllegalArgumentException("해당 데이터가 없습니다."));
         Board board = boardRepository.findById(likesRequestDto.getBoardId()).orElseThrow(() -> new IllegalArgumentException("해당 데이터가 없습니다."));
 
-        Long likesId = likesRepository.save(Likes.builder().user(user).board(board).build()).getId();
+        Likes likes = likesRepository.save(Likes.builder().user(user).board(board).build());
 
-        return likesId;
+        return new LikesResponseDto(likes);
     }
 
     /** 삭제 - likes 한 개 */
-    @Transactional
     public Long delete(Long userId, Long boardId){
         likesRepository.deleteByUserIdAndBoardId(userId, boardId);
         em.flush();
         em.clear();
 
-        // 검토 - (임시) return userId
+        // 검토 - (임시) 200L
         return 200L;
     }
 }
