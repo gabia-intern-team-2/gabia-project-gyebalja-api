95e90e13b9909009a7dd220eba12e866686454fd
JTG94 <sky_engel@naver.com>
2020-02-10 21:48:29 +0900

    Api 작성 - Category (테스트코드) 중간 커밋

---
+++ src/test/java/com/gabia/gyebalja/category/CategoryServiceTest.java
@@ -0,0 +1,110 @@
+package com.gabia.gyebalja.category;
+
+import com.gabia.gyebalja.domain.Category;
+import com.gabia.gyebalja.dto.category.CategoryRequestDto;
+import com.gabia.gyebalja.dto.category.CategoryResponseDto;
+import com.gabia.gyebalja.repository.CategoryRepository;
+import com.gabia.gyebalja.service.CategoryService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+public class CategoryServiceTest {
+
+    @PersistenceContext
+    EntityManager em;
+    @Autowired
+    CategoryService categoryService;
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    /**
+     * 등록 - category 한 건 (단건 등록)
+     */
+    @Test
+    @DisplayName("CategoryService.postOneCategory() 테스트 (단건 저장)")
+    public void postOneCateogry() throws Exception {
+        //given
+        CategoryRequestDto categoryRequestDto = CategoryRequestDto.builder()
+                                                                .name("개발")
+                                                                .build();
+        //when
+        Long categoryId = categoryService.postOneCategory(categoryRequestDto);
+        em.clear();
+        Category findCategory = categoryRepository.findById(categoryId).get();
+        //then
+        assertThat(categoryId).isEqualTo(findCategory.getId());
+        assertThat(categoryRequestDto.getName()).isEqualTo(findCategory.getName());
+    }
+
+    /**
+     * 조회 - category 한 건 (단건 조회)
+     */
+    @Test
+    @DisplayName("CategoryService.getOneCategory() 테스트 (단건 조회)")
+    public void getOneCategory() throws Exception {
+        //given
+        CategoryRequestDto categoryRequestDto = CategoryRequestDto.builder()
+                                                                .name("개발")
+                                                                .build();
+        Long categoryId = categoryService.postOneCategory(categoryRequestDto);
+        em.clear();
+        //when
+        CategoryResponseDto findCategory = categoryService.getOneCategory(categoryId);
+        //then
+        assertThat(findCategory.getId()).isEqualTo(categoryId);
+        assertThat(findCategory.getName()).isEqualTo(categoryRequestDto.getName());
+    }
+
+    /**
+     * 수정 - category 한 건 (단건 수정)
+     */
+    @Test
+    @DisplayName("CategoryService.putOneCategory() 테스트 (단건 수정)")
+    public void putOneCategory() throws Exception {
+        //given
+        String updateName = "기획";
+        CategoryRequestDto categoryRequestDto = CategoryRequestDto.builder()
+                .name("개발")
+                .build();
+        Long categoryId = categoryService.postOneCategory(categoryRequestDto);
+        //when
+        categoryRequestDto.setName(updateName);
+        Long updateId = categoryService.putOneCategory(categoryId, categoryRequestDto);
+        em.flush(); //업데이트 쿼리를 보기위함.
+        Category findUpdateCategory = categoryRepository.findById(updateId).get();
+        //then
+        assertThat(findUpdateCategory.getId()).isEqualTo(categoryId);
+        assertThat(findUpdateCategory.getName()).isEqualTo(updateName);
+    }
+
+    /**
+     * 삭제 - category 한 건(단건 삭제)
+     */
+    @Test
+    public void deleteOneCategory() throws Exception {
+        //given
+        CategoryRequestDto categoryRequestDto = CategoryRequestDto.builder()
+                .name("개발")
+                .build();
+        Long categoryId = categoryService.postOneCategory(categoryRequestDto);
+        long beforeDeleteCnt = categoryRepository.count();
+        //when
+        Long deleteId = categoryService.deleteOneCategory(categoryId);
+        //then
+        assertThat(categoryRepository.count()).isEqualTo(beforeDeleteCnt-1);
+        assertThat(categoryRepository.findById(deleteId)).isEqualTo(Optional.empty());
+    }
+
+}
