ae18045f67c613f6a999e4ada6cffe6737d962e7
LEEHYUNJAE <35790290+hjjae2@users.noreply.github.com>
2020-02-11 10:05:36 +0900

    #121 Api 작성 - Board - '좋아요 개수' 세팅 로직 추가, Board 테스트 코드 수정 (#125)
    * #121 Api 작성 - Board - '좋아요 개수' 세팅 로직 추가, Board 테스트 코드 수정

--- src/main/java/com/gabia/gyebalja/service/BoardService.java
+++ src/main/java/com/gabia/gyebalja/service/BoardService.java
@@ -7,6 +7,7 @@ import com.gabia.gyebalja.dto.board.BoardResponseDto;
 import com.gabia.gyebalja.dto.comment.CommentResponseDto;
 import com.gabia.gyebalja.repository.BoardRepository;
 import com.gabia.gyebalja.repository.CommentRepository;
+import com.gabia.gyebalja.repository.LikesRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -27,34 +28,39 @@ public class BoardService {
 
     private final BoardRepository boardRepository;
     private final CommentRepository commentRepository;
+    private final LikesRepository likesRepository;
 
 
     /** 등록 - board 한 건 (게시글 등록) */
     @Transactional
     public Long postOneBoard(BoardRequestDto boardRequestDto){
-        Long boardId = boardRepository.save(boardRequestDto.toEntity()).getId(); // 검토. getId() 적절한지?
+        Long boardId = boardRepository.save(boardRequestDto.toEntity()).getId();
 
         return boardId;
     }
 
     /** 조회 - board 한 건 (상세페이지) */
     @Transactional
-    public BoardResponseDto getOneBoard(Long id){
-        Board board = boardRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));    // 검토. 404 Error?
+    public BoardResponseDto getOneBoard(Long boardId){
+        Board board = boardRepository.findById(boardId).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));
         BoardResponseDto boardResponseDto = new BoardResponseDto(board);
 
-        // 게시글에 속한 댓글 조회, boardDto 에 삽입
-        List<Comment> comments = commentRepository.findByBoardId(id);
+        // 게시글의 댓글 조회, boardDto 에 삽입
+        List<Comment> comments = commentRepository.findByBoardId(boardId);
         List<CommentResponseDto> commentResponseDtos = comments.stream().map(comment -> new CommentResponseDto(comment)).collect(Collectors.toList());
         boardResponseDto.changeCommentList(commentResponseDtos);
+
+        // 게시글의 좋아요 조회, boardDto 에 삽입
+        int totalNumberOfLikes = likesRepository.countByBoardId(boardId);
+        boardResponseDto.changeLikes(totalNumberOfLikes);
 
         return boardResponseDto;
     }
 
     /** 수정 - board 한 건 (상세페이지에서) */
     @Transactional
-    public Long putOneBoard(Long id, BoardRequestDto boardRequestDtoDto){
-        Board board = boardRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));    // 검토. 404 Error?
+    public Long putOneBoard(Long boardId, BoardRequestDto boardRequestDtoDto){
+        Board board = boardRepository.findById(boardId).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));
 
         // 더티 체킹
         board.changeTitle(boardRequestDtoDto.getTitle());
@@ -62,25 +68,26 @@ public class BoardService {
         em.flush();
         em.clear();
 
-        return id;
+        return boardId;
     }
 
     /** 삭제 - board 한 건 (상세페이지에서) */
     @Transactional
-    public Long deleteOneBoard(Long id){
-        boardRepository.deleteById(id);
+    public Long deleteOneBoard(Long boardId){
+        boardRepository.deleteById(boardId);
         em.flush();
         em.clear();
 
-        return id;
+        return boardId;
     }
 
     /** 조회 - board 전체 (페이징) */
     @Transactional
     public Page<BoardResponseDto> getAllBoard(Pageable pageable){
+        // 추후, 필요에 따라 댓글 개수, 좋아요 개수 등을 삽입하는 로직 추가
         Page<Board> boardPage = boardRepository.findAll(pageable);
-        Page<BoardResponseDto> boardDtoPage = boardPage.map(board -> new BoardResponseDto(board));  // 검토. stream?
+        Page<BoardResponseDto> boardResponseDtoPage = boardPage.map(board -> new BoardResponseDto(board));
 
-        return boardDtoPage;
+        return boardResponseDtoPage;
     }
 }
