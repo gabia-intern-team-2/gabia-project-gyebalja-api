c29915b2dc7cc1dc436751156530dd5c6e1fb903
LEEHYUNJAE <hjjae2@gmail.com>
2020-02-21 16:50:37 +0900

    #166 Api 작성-Department 전체조회

--- src/main/java/com/gabia/gyebalja/controller/DepartmentApiController.java
+++ src/main/java/com/gabia/gyebalja/controller/DepartmentApiController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 public class DepartmentApiController {
@@ -28,4 +30,12 @@ public class DepartmentApiController {
     /** 수정 - department (한 부서) */
 
     /** 삭제 - department (한 부서) */
+
+    /** 조회 - department (전체) */
+    @GetMapping("/api/v1/departments")
+    public CommonJsonFormat getAllDepartment(){
+        List<DepartmentResponseDto> response = departmentService.getAllDepartment();
+
+        return new CommonJsonFormat(StatusCode.OK.getCode(), StatusCode.OK.getMessage(), response);
+    }
 }
