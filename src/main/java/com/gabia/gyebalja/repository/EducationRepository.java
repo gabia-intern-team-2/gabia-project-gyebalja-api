08fe875ee1279744bd98a9b7330bdd568f20c1e4
LEEHYUNJAE <hjjae2@gmail.com>
2020-02-24 17:35:47 +0900

    #168 코드 정리

--- src/main/java/com/gabia/gyebalja/repository/EducationRepository.java
+++ src/main/java/com/gabia/gyebalja/repository/EducationRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,28 +21,4 @@ public interface EducationRepository extends JpaRepository<Education,Long> {
     @Query("select e from Education e join fetch e.category c where e.user.id = :userId")
     List<Education> findEducationByUserId(@Param("userId") Long userId, Pageable pageable);
 
-    /** 통계 - 메인 화면 */
-    /** 연도별 교육 건수, 시간 */
-    // 방법 1
-    @Query("select count(e), sum(e.totalHours) from Education e where substring(e.startDate, 1, 4) in :year")
-    List<ArrayList<Long>> getMainStatisticsWithYear1(@Param("year") List<String> year);
-
-    // 방법 2
-    @Query("select substring(e.startDate, 6, 2) as year, count(e) as totalEducationNumberOfEmployees, sum(e.totalHours) as totalEducationHourOfEmployees " +
-            "from Education e " +
-            "group by substring(e.startDate, 6, 2) " +
-            "order by substring(e.startDate, 6, 2) desc")
-    List<ArrayList<Long>> getMainStatisticsWithYear2(Pageable pageable);
-
-    /** 월별 교육 건수, 시간 */
-    // 방법 1
-    @Query("select substring(e.startDate, 1 ,7) as month, count(e) as totalEducationNumberOfEmployees, sum(e.totalHours) as totalEducationHourOfEmployees " +
-            "from Education e " +
-            "group by substring(e.startDate, 1, 7) " +
-            "order by substring(e.startDate, 1, 7) desc")
-    List<ArrayList<String>> getMainStatisticsWithMonth(Pageable pageable);
-
-    /** 카테고리 TOP n */
-    @Query(value = "select c.name, count(e) from Education e join e.category c group by c.id order by count(e) desc")
-    List<ArrayList<String>> getMainStatisticsWithCategory(Pageable pageable);
 }
