037c5615af9de0dd9a0b9164d700814576a6fcd1
LEEHYUNJAE <hjjae2@gmail.com>
2020-02-24 17:55:00 +0900

    #168 Api 작성 - 통계 (메인)

--- src/main/java/com/gabia/gyebalja/repository/EduTagRepository.java
+++ src/main/java/com/gabia/gyebalja/repository/EduTagRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-
 public interface EduTagRepository extends JpaRepository<EduTag, Long> {
     @Modifying
     @Transactional
