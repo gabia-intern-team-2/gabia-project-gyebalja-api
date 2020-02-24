a555a27d864e7b2daf5f0bae3d09ade907c9f39c
TaeGyun-Jung <wjdxorbs010@gmail.com>
2020-02-24 09:44:36 +0900

    #170 Education Api 피드백 반영 - EduTag 리포지토리 쿼리 메서드 수정 및 불필요 import 제거

--- src/main/java/com/gabia/gyebalja/repository/EduTagRepository.java
+++ src/main/java/com/gabia/gyebalja/repository/EduTagRepository.java
@@ -2,11 +2,7 @@ package com.gabia.gyebalja.repository;
 
 import com.gabia.gyebalja.domain.EduTag;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface EduTagRepository extends JpaRepository<EduTag, Long> {
-    void deleteByEducationId(@Param("educationId") Long educationId);
+    void deleteByEducationId(Long educationId);
 }
