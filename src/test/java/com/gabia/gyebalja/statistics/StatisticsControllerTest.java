cd2aefb8167285dfb4620a0b069281a16c014f25
LEEHYUNJAE <hjjae2@gmail.com>
2020-02-26 16:47:52 +0900

    #185 메인 통계 - 컨트롤러 테스트 작성 완료

---
+++ src/test/java/com/gabia/gyebalja/statistics/StatisticsControllerTest.java
@@ -0,0 +1,37 @@
+package com.gabia.gyebalja.statistics;
+
+import com.gabia.gyebalja.common.CommonJsonFormat;
+import com.gabia.gyebalja.common.StatusCode;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StatisticsControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void getMainStatistics() {
+        // given
+        String url = "http://localhost:" + port + "/api/v1/statistics/main";
+
+        // then
+        ResponseEntity<CommonJsonFormat> responseEntity = restTemplate.getForEntity(url, CommonJsonFormat.class);
+
+        // when
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody().getCode()).isEqualTo(StatusCode.OK.getCode());
+        assertThat(responseEntity.getBody().getMessage()).isEqualTo(StatusCode.OK.getMessage());
+    }
+}
