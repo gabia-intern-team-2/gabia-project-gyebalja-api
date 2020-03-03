b47762a52af8c051f8493dc74ff2bda006bb9a50
LEEHYUNJAE <35790290+hjjae2@users.noreply.github.com>
2020-03-03 15:37:38 +0900

    #198 BoardImg 구현 (#199)
    * #198 BoardImg 컨트롤러 작성, WebConfig 작성
    
    * #198 resource 제거

---
+++ src/main/java/com/gabia/gyebalja/WebConfig.java
@@ -0,0 +1,26 @@
+package com.gabia.gyebalja;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/images/**")
+                .addResourceLocations("classpath:/static/images/")
+                .setCachePeriod(20);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}
