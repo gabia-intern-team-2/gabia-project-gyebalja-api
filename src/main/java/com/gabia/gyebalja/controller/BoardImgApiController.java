3d4f0881885bd1b26a124f99a3585586a9f33424
LEEHYUNJAE <hjjae2@gmail.com>
2020-03-06 18:02:22 +0900

    BoardImg Controller Service 분리

--- src/main/java/com/gabia/gyebalja/controller/BoardImgApiController.java
+++ src/main/java/com/gabia/gyebalja/controller/BoardImgApiController.java
@@ -1,36 +1,25 @@
 package com.gabia.gyebalja.controller;
 
+import com.gabia.gyebalja.service.BoardImgService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 
+@RequiredArgsConstructor
 @RestController
 public class BoardImgApiController {
 
-    public static final String uploadingDir = "/var/www/html/images/";
+    private final BoardImgService boardImgService;
 
     /** 등록 - boardImg 한 건 (이미지 등록) */
     @PostMapping("/api/v1/boardImgs")
     public String postOneBoardImg(@RequestParam("image") MultipartFile image) throws IOException {
-        int tryCount = 0;
-        int tryThreshold = 100;
-        int randomNumberRange = 10;
+        String response = boardImgService.postOneBoardImg(image);
 
-        String fileName = System.currentTimeMillis() + ((int) Math.random() * randomNumberRange) + image.getOriginalFilename();
-        File file = new File(uploadingDir + fileName);
-        for(; file.exists() && tryCount < tryThreshold; tryCount++){
-            fileName = System.currentTimeMillis() + ((int) Math.random() * randomNumberRange) + image.getOriginalFilename();
-            file = new File(uploadingDir + fileName);
-        }
-        if(tryCount >= tryThreshold) return "images/uploadfailed.jpg";
-
-        image.transferTo(file);
-        String imageUrl = "images/" + fileName;
-
-        return imageUrl;
+        return response;
     }
 }
