064bd866328c44d19a8e8c11cd292037f31413a4
LEEHYUNJAE <hjjae2@gmail.com>
2020-03-06 13:59:31 +0900

    #210 boardImg 업로드 파일명 랜덤하게 설정

--- src/main/java/com/gabia/gyebalja/controller/BoardImgApiController.java
+++ src/main/java/com/gabia/gyebalja/controller/BoardImgApiController.java
@@ -7,21 +7,32 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @RestController
 public class BoardImgApiController {
 
-//    public static final String uploadingDir = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\images\\";
+    //    public static final String uploadingDir = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\images\\";
     public static final String uploadingDir = "/var/www/html/images/";
 
     /** 등록 - boardImg 한 건 (이미지 등록) */
     @PostMapping("/api/v1/boardImgs")
     public String postOneBoardImg(@RequestParam("image") MultipartFile image) throws IOException {
-        byte[] data = image.getBytes();
-        File file = new File(uploadingDir + image.getOriginalFilename());
-        image.transferTo(file);
+        int tryCount = 0;
+        int tryThreshold = 100;
+        int randomNumberRange = 10;
 
-        String imageUrl = "images/" + image.getOriginalFilename();
+        String fileName = System.currentTimeMillis() + ((int) Math.random() * randomNumberRange) + image.getOriginalFilename();
+        File file = new File(uploadingDir + fileName);
+        for(; file.exists() && tryCount < tryThreshold; tryCount++){
+            fileName = System.currentTimeMillis() + ((int) Math.random() * randomNumberRange) + image.getOriginalFilename();
+            file = new File(uploadingDir + fileName);
+        }
+        if(tryCount >= tryThreshold) return "images/uploadfailed.jpg";
+
+        image.transferTo(file);
+        String imageUrl = "images/" + fileName;
 
         return imageUrl;
     }
