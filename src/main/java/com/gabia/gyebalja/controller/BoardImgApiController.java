30f479aaedac062c696eb9cb3dc594108fa9bf41
LEEHYUNJAE <hjjae2@gmail.com>
2020-03-06 14:00:30 +0900

    #210 boardImg 불필요 import 삭제

--- src/main/java/com/gabia/gyebalja/controller/BoardImgApiController.java
+++ src/main/java/com/gabia/gyebalja/controller/BoardImgApiController.java
@@ -7,8 +7,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 @RestController
 public class BoardImgApiController {
