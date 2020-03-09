27b949a2666b2a43c7bf6fafbadf585b7a969821
JTG94 <48273572+JTG94@users.noreply.github.com>
2020-03-09 16:08:37 +0900

    #221 Api 작성 - 사용자 프로필 이미지 업로드 기능 (#222)
    * #221 Api 작성 - 사용자 프로필 이미지 업로드 기능
    
    * #221 ImageService 수정
    
    Co-authored-by: TaeGyun-Jung <wjdxorbs010@gmail.com>

---
+++ src/main/java/com/gabia/gyebalja/service/ImageService.java
@@ -0,0 +1,54 @@
+package com.gabia.gyebalja.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Transactional
+@Service
+public class ImageService {
+
+    public static final String boardImgDir = "/var/www/html/images/boards/";
+    public static final String userImgDir = "/var/www/html/images/users/";
+
+    public String postOneBoardImg(MultipartFile image) throws IOException {
+        int tryCount = 0;
+        int tryThreshold = 100;
+        int randomNumberRange = 10;
+
+        String fileName = System.currentTimeMillis() + ((int) Math.random() * randomNumberRange) + image.getOriginalFilename();
+        File file = new File(boardImgDir + fileName);
+        for(; file.exists() && tryCount < tryThreshold; tryCount++){
+            fileName = System.currentTimeMillis() + ((int) Math.random() * randomNumberRange) + image.getOriginalFilename();
+            file = new File(boardImgDir + fileName);
+        }
+        if(tryCount >= tryThreshold) return "http://api.gyeblja.com/images/boards/uploadfailed.jpg";
+
+        image.transferTo(file);
+        String imageUrl = "http://api.gyeblja.com/images/boards/" + fileName;
+
+        return imageUrl;
+    }
+
+    public String postOneUserImg(MultipartFile image) throws IOException {
+        int tryCount = 0;
+        int tryThreshold = 100;
+        int randomNumberRange = 10;
+
+        String fileName = System.currentTimeMillis() + ((int) Math.random() * randomNumberRange) + image.getOriginalFilename();
+        File file = new File(userImgDir + fileName);
+        for(; file.exists() && tryCount < tryThreshold; tryCount++){
+            fileName = System.currentTimeMillis() + ((int) Math.random() * randomNumberRange) + image.getOriginalFilename();
+            file = new File(userImgDir + fileName);
+        }
+        if(tryCount >= tryThreshold) return "http://api.gyeblja.com/images/users/basic.jpg";
+
+        image.transferTo(file);
+        String imageUrl = "http://api.gyeblja.com/images/users/" + fileName;
+
+        return imageUrl;
+    }
+}
