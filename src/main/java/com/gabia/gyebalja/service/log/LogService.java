7ca8d4ac239e2180a11d8ba2ba4c034d690b60a3
LEEHYUNJAE <35790290+hjjae2@users.noreply.github.com>
2020-03-11 11:40:25 +0900

    #219 Logger - 기능 구현 (#229)
    * #219 gitignore logs 폴더 추가
    
    * #219 logback
    
    * #219 주석 위치 변경

---
+++ src/main/java/com/gabia/gyebalja/service/log/LogService.java
@@ -0,0 +1,23 @@
+package com.gabia.gyebalja.service.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+// 참고
+// https://meetup.toast.com/posts/149
+// https://www.sangkon.com/hands-on-springboot-logging/
+
+@Service
+public class LogService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
+    public void log() {
+        logger.trace("Gyebalja-Trace");
+        logger.debug("Gyebalja-Debug");
+        logger.info("Gyebalja-Info");
+        logger.warn("Gyebalja-Warn");
+        logger.error("Gyebalja-Error");
+    }
+}
