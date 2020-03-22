dc4bb1934f2cdced87076e2e47f8b03253f24a3d
JTG94 <sky_engel@naver.com>
2020-03-22 14:24:43 +0900

    #287 스웨거 적용 - 해당 API 상태 코드 및 설명 작성

--- src/main/java/com/gabia/gyebalja/controller/LikesApiController.java
+++ src/main/java/com/gabia/gyebalja/controller/LikesApiController.java
@@ -5,6 +5,10 @@ import com.gabia.gyebalja.common.StatusCode;
 import com.gabia.gyebalja.dto.likes.LikesRequestDto;
 import com.gabia.gyebalja.dto.likes.LikesResponseDto;
 import com.gabia.gyebalja.service.LikesService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,12 +23,19 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RequiredArgsConstructor
+@Api(value = "LikesApiController V1")
 @RestController
 public class LikesApiController {
 
     private final LikesService likesService;
 
     /** 등록 - likes 한 개 */
+    @ApiOperation(value = "postOneLikes : 등록 - likes 한 개", notes = "좋아요 등록 요청")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "BAD_REQUEST"),
+            @ApiResponse(code = 500, message = "INTERNAL_SERVER_ERROR")
+    })
     @PostMapping("/api/v1/likes")
     public CommonJsonFormat postOneLikes(@RequestBody LikesRequestDto likesRequestDto){
         Long response = likesService.postOneLikes(likesRequestDto);
@@ -33,6 +44,12 @@ public class LikesApiController {
     }
 
     /** 조회 - likes 한 개 */
+    @ApiOperation(value = "getOneLikes : 조회 - likes 한 개", notes = "좋아요 조회 요청")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "BAD_REQUEST"),
+            @ApiResponse(code = 500, message = "INTERNAL_SERVER_ERROR")
+    })
     @GetMapping("/api/v1/likes/users/{userId}/boards/{boardId}")
     public CommonJsonFormat getOneLikes(@PathVariable("userId") Long userId, @PathVariable("boardId") Long boardId){
         LikesResponseDto response = likesService.getOneLikes(userId, boardId);
@@ -41,6 +58,12 @@ public class LikesApiController {
     }
 
     /** 삭제 - likes 한 개 */
+    @ApiOperation(value = "deleteOneLikes : 삭제 - likes 한 개", notes = "좋아요 취소(삭제) 요청")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "BAD_REQUEST"),
+            @ApiResponse(code = 500, message = "INTERNAL_SERVER_ERROR")
+    })
     @DeleteMapping("/api/v1/likes/users/{userId}/boards/{boardId}")
     public CommonJsonFormat deleteOneLikes(@PathVariable("userId") Long userId, @PathVariable("boardId") Long boardId){
         Long response = likesService.deleteOneLikes(userId, boardId);
--- src/main/java/com/gabia/gyebalja/controller/TagApiController.java
+++ src/main/java/com/gabia/gyebalja/controller/TagApiController.java
@@ -5,6 +5,10 @@ import com.gabia.gyebalja.common.StatusCode;
 import com.gabia.gyebalja.dto.tag.TagRequestDto;
 import com.gabia.gyebalja.dto.tag.TagResponseDto;
 import com.gabia.gyebalja.service.TagService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,12 +26,19 @@ import java.util.List;
  */
 
 @RequiredArgsConstructor
+@Api(value = "TagApiController V1")
 @RestController
 public class TagApiController {
 
     private final TagService tagService;
 
     /** 등록 - tag 한 건 (태그 등록) */
+    @ApiOperation(value = "postOneTag : 등록 - Tag 한 건", notes = "Tag 저장 요청")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "BAD_REQUEST"),
+            @ApiResponse(code = 500, message = "INTERNAL_SERVER_ERROR")
+    })
     @PostMapping("/api/v1/tags")
     public CommonJsonFormat postOneTag(@RequestBody TagRequestDto tagRequestDto) {
         Long tagId = tagService.postOneTag(tagRequestDto);
@@ -36,6 +47,12 @@ public class TagApiController {
     }
 
     /** 조회 - tag 한 건 (태그 단건 조회) */
+    @ApiOperation(value = "getOneTag : 조회 - Tag 한 건", notes = "Tag의 Sequence 넘버로 조회 요청")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "BAD_REQUEST"),
+            @ApiResponse(code = 500, message = "INTERNAL_SERVER_ERROR")
+    })
     @GetMapping("/api/v1/tags/{tagId}")
     public CommonJsonFormat getOneTag(@PathVariable("tagId") Long id) {
         TagResponseDto tagResponseDto = tagService.getOneTag(id);
@@ -44,6 +61,12 @@ public class TagApiController {
     }
 
     /** 수정 - tag 한 건 (태그 단건 수정) */
+    @ApiOperation(value = "putOneTag : 수정 - Tag 한 건", notes = "Tag 정보 수정 요청")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "BAD_REQUEST"),
+            @ApiResponse(code = 500, message = "INTERNAL_SERVER_ERROR")
+    })
     @PutMapping("/api/v1/tags/{tagId}")
     public CommonJsonFormat putOneTag(@PathVariable("tagId") Long id, @RequestBody TagRequestDto tagRequestDto) {
         Long tagId = tagService.putOneTag(id, tagRequestDto);
@@ -52,6 +75,12 @@ public class TagApiController {
     }
 
     /** 삭제 - tag 한 건 (태그 단건 삭제) */
+    @ApiOperation(value = "deleteOneTag : 삭제 - Tag 한 건", notes = "Tag 정보 삭제 요청")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "BAD_REQUEST"),
+            @ApiResponse(code = 500, message = "INTERNAL_SERVER_ERROR")
+    })
     @DeleteMapping("/api/v1/tags/{tagId}")
     public CommonJsonFormat deleteOneTag(@PathVariable("tagId") Long id) {
         Long tagId = tagService.deleteOneTag(id);
@@ -60,6 +89,12 @@ public class TagApiController {
     }
 
     /** 조회 - tag 전체 조회 (페이징 x) */
+    @ApiOperation(value = "getAllTag : 조회 - Tag 전체 조회", notes = "Tag 정보 전체 요청")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "BAD_REQUEST"),
+            @ApiResponse(code = 500, message = "INTERNAL_SERVER_ERROR")
+    })
     @GetMapping("/api/v1/tags")
     public CommonJsonFormat getAllTag() {
         List<TagResponseDto> allTag = tagService.getAllTag();
